963a2a1e0a9d086b91f375cd76d4303a9b4a4a96
Kristopher <kristopher_matic@hotmail.com>
2023-04-03 19:22:23 -0700

    Add marker to map

--- app/record.tsx
+++ app/record.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import { useEffect, useState } from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 import * as Location from "expo-location";
 
 const Record = () => {
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
-
-    const getCurrentLocation = async () => {};
 
     useEffect(() => {
         (async () => {
@@ -21,24 +19,29 @@ const Record = () => {
 
             const location = await Location.getCurrentPositionAsync({});
             console.log(location);
-            console.log(location.coords.latitude);
             setLocation(location);
         })();
     }, []);
 
     return (
         <View style={styles.container}>
-            <Text>Run</Text>
             <MapView
                 style={styles.map}
                 region={{
                     latitude: location?.coords.latitude!,
                     longitude: location?.coords.longitude!,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
                 }}
-            ></MapView>
-            <Pressable style={styles.record} onPress={() => getCurrentLocation()}>
+            >
+                <Marker
+                    coordinate={{
+                        latitude: location?.coords.latitude!,
+                        longitude: location?.coords.longitude!,
+                    }}
+                />
+            </MapView>
+            <Pressable style={styles.record} onPress={() => console.log("yeet")}>
                 <Text style={{ color: "white", fontSize: 18 }}>Start</Text>
             </Pressable>
             <StatusBar style="auto" />
@@ -51,21 +54,22 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
-        justifyContent: "space-around",
+        justifyContent: "space-between",
     },
 
     record: {
         backgroundColor: "red",
-        width: 100,
-        height: 100,
+        width: 90,
+        height: 90,
         alignItems: "center",
         justifyContent: "center",
-        borderRadius: 50,
+        borderRadius: 45,
+        marginBottom: 10,
     },
 
     map: {
         width: "100%",
-        height: "75%",
+        height: "80%",
     },
 });
 
